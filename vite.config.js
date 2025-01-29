1d896ca93c5200f251cc335b466721ca0497611c
Milagros <milagrospeqque19@gmail.com>
2025-01-29 08:36:04 -0500

    add

--- vite.config.js
+++ vite.config.js
@@ -10,9 +10,10 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
+  base: 'https://github.com/hazielace.github.io/ReportsTkvue'
+  // resolve: {
+  //   alias: {
+  //     '@': fileURLToPath(new URL('./src', import.meta.url))
+  //   },
+  // },
 })
